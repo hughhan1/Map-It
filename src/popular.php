88089d7df8a7a516ab550d1d7afa5f4fae74e8c8
Hugh Han <hughhan1@gmail.com>
2015-06-01 22:56:08 -0400

    Small fix

--- src/popular.php
+++ src/popular.php
@@ -16,13 +16,13 @@ $result = $instagram->getPopularMedia();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instagram - popular photos</title>
     <link href="https://vjs.zencdn.net/4.2/video-js.css" rel="stylesheet">
-    <link href="assets/style.css" rel="stylesheet">
+    <link href="../assets/style.css" rel="stylesheet">
     <script src="https://vjs.zencdn.net/4.2/video.js"></script>
 </head>
 <body>
 <div class="container">
     <header class="clearfix">
-        <img src="assets/instagram.png" alt="Instagram logo">
+        <img src="../assets/instagram.png" alt="Instagram logo">
 
         <h1>Instagram <span>popular photos</span></h1>
     </header>
